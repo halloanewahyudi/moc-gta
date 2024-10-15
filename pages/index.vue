8c5444063c2d9049dc20699b4b773539097b3256
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2024-10-15 10:31:10 +0700

    rev

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 <SectionsHero class="mb-16"/>
 <SectionsSecTwo class="mb-20" />
-<SectionsSecThree class="mb-16" />
+<SectionsSecThree class="mb-20" />
 <SectionsSecFour class="mb-16" />
 </template>
